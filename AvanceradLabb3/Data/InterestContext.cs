389ee9eb93cfa27e34c16dba3143eaf02b8d98aa
GoodStuff15 <gustav.emil.eriksson@gmail.com>
2025-04-22 15:40:31 +0200

    DbContext and initial seeds are done, not tested!

---
+++ AvanceradLabb3/Data/InterestContext.cs
@@ -0,0 +1,99 @@
+﻿using AvanceradLabb3.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace AvanceradLabb3.Data
+{
+    public class InterestContext : DbContext
+    {
+        public DbSet<Person> People => Set<Person>();
+
+        public DbSet<Interest> Interests => Set<Interest>();
+
+        public InterestContext(DbContextOptions<InterestContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            SeedData(modelBuilder);
+        }
+
+        protected void SeedData(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().HasData(
+
+                new Person
+                {
+                    Id = 1,
+                    FirstName = "Gustav",
+                    LastName = "Eriksson",
+                    Email = "gustav@swedbonk.se",
+                    Age = 42
+                },
+                new Person
+                {
+                    Id = 2,
+                    FirstName = "McGyver",
+                    LastName = "Jonsson",
+                    Email = "mackans@bygghemma.se",
+                    Age = 72
+                },                
+                new Person
+                {
+                    Id = 3,
+                    FirstName = "Jean Claude",
+                    LastName = "Van Damme",
+                    Email = "jean-claude.vandamme@swipnet.se",
+                    Age = 63
+                }
+
+            );
+
+            modelBuilder.Entity<Interest>().HasData(
+
+                new Interest
+                {
+                    Id = 1,
+                    Title = "Magic The Gathering",
+                    Description = "A collectible card game, and its rewarding and strategic gameplay, compelling characters, and fantastic Multiverse have entertained and delighted fans for more than 30 years. With more than 50 million fans to date, MAGIC is a worldwide phenomenon published in more than 150 countries."
+                },
+                new Interest
+                {
+                    Id = 2,
+                    Title = "Mecka",
+                    Description = "Skruva och ha sig lite"
+                },
+                new Interest
+                {
+                    Id = 3,
+                    Title = "Slåss",
+                    Description = "Slåss pang bom krasch!!"
+                }
+
+            );
+
+            modelBuilder.Entity<Person>(p =>
+            {
+                p.HasMany(p => p.Interests)
+                .WithMany(i => i.People)
+                .UsingEntity(
+                    "PersonInterest",
+                    pi =>
+                    {
+                        pi.HasKey("PersonId", "InterestId");
+                        pi.HasData(
+                            new { PersonId = 1, InterestId = 1 },
+                            new { PersonId = 2, InterestId = 2 },
+                            new { PersonId = 2, InterestId = 3 },
+                            new { PersonId = 3, InterestId = 3 }
+                        );
+                    }
+                );
+            });
+        }
+    }
+}
