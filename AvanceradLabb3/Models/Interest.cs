9991ffed672425f76fdbc1e7fb422989a479a894
GoodStuff15 <gustav.emil.eriksson@gmail.com>
2025-04-22 15:19:23 +0200

    Added models for Person and Interest

---
+++ AvanceradLabb3/Models/Interest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AvanceradLabb3.Models
+{
+    public class Interest
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title {  get; set; }
+        [Required]
+        public string Description { get; set; }
+
+        // Navigation properties
+        public ICollection<Person>? People { get; set; }
+    }
+}
---
+++ AvanceradLabb3/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AvanceradLabb3.Models
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+
+        public string? Email { get; set; }
+        public int? Age { get; set; }
+
+        // Navigation properties
+        public ICollection<Interest>? Interests { get; set; }
+    }
+}
