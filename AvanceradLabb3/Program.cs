aac4dd8fd85e79d2bcc5d62804b6d4b73f0d3a4a
GoodStuff15 <gustav.emil.erikson@gmail.com>
2025-04-23 11:03:03 +0200

    Base of controller added

--- AvanceradLabb3/Program.cs
+++ AvanceradLabb3/Program.cs
@@ -40,3 +40,57 @@ namespace AvanceradLabb3
         }
     }
 }
+
+/*
+ 
+<aside>
+?? **Applikationen/databasen**
+
+Din första uppgift är att skapa ett grundläggande API med en databas som har följande funktioner:
+
+- [ ]  Det ska gå att lagra personer med grundläggande information om dem som namn och telefonnummer
+- [ ]  Systemet ska kunna lagra ett obegränsat antal intressen. Varje intresse ska ha en titel och en kort beskrivning
+- [ ]  Varje person ska kunna kopplas till valfritt antal intressen
+- [ ]  Det ska gå att lagra ett obegränsat antal länkar till webbplatser för varje intresse och person. En länk är alltid kopplad till både personen och intresset
+</aside>
+
+<aside>
+?? **Skapa ett REST-API**
+
+Det andra steget är att skapa ett REST-API som låter externa tjänster utföra följande operationer i din applikation:
+
+- [ ]  Hämta alla personer i systemet
+- [ ]  Hämta alla intressen kopplade till en specifik person
+- [ ]  Hämta alla länkar kopplade till en specifik person
+- [ ]  Koppla en person till ett nytt intresse
+- [ ]  Lägga till nya länkar för en specifik person och ett specifikt intresse
+</aside>
+
+<aside>
+<img src="/icons/zoom-in_green.svg" alt="/icons/zoom-in_green.svg" width="40px" /> **Extra utmaning (gör om du vill)**
+
+- [ ]  Gör det möjligt för API-anrop som hämtar en person att direkt få ut alla personens intressen och länkar i en hierarkisk JSON-struktur
+- [ ]  Lägg till filtreringsfunktion i API:et. Till exempel, om man skickar med "to" vid hämtning av personer ska man få träffar på namn som innehåller "to", som "Tobias" eller "Tomas". Du kan implementera denna funktion för alla API-anrop om du vill.
+- [ ]  Implementera paginering i API:et. Till exempel, vid hämtning av personer kan man få de första 100 personerna och behöver göra ytterligare anrop för att få fler. Låt anropet specificera hur många personer som ska returneras per sida – användaren kan då välja att få exempelvis 10 personer per anrop.
+</aside>
+
+<aside>
+??? **Testa ditt API**
+
+Det sista steget är att testa ditt API genom antingen [Postman](https://www.postman.com/) eller Swagger.
+
+- [ ]  Gör ett testanrop för varje API-krav som listats ovan
+- [ ]  Dokumentera alla testanrop i din readme-fil på GitHub så att vi kan se din tänkta struktur för API-anropen
+</aside>
+
+<aside>
+?
+
+**För VG**
+
+- [ ]  Skriv en reflektion över hur du har tillämpat SOLID principles i din applikation, lämna in denna som en pdf tillsammans med länken till ditt GitHub-repo på LearnPoint.
+</aside>
+ 
+ */
+
+// Repository pattern - klass som sköter kommunikation med databasen
